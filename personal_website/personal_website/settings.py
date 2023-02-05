bb9023c5ad1687b4484af771df48a9859f36e794
simon <93simonster@gmail.com>
2023-02-05 12:28:20 +0100

    csrf

--- personal_website/personal_website/settings.py
+++ personal_website/personal_website/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = 1#int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
+CSRF_TRUSTED_ORIGINS = ['https://martin.simoon.duckdns.org']
 
 # Application definition
 
